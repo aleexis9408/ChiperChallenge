06aed591272f578d0433becb91fce920c6905045
Alexis R <alexis.rojas@pragma.com.co>
2022-02-23 18:17:43 -0500

    styles cards

--- src/components/molecules/card-post/card-post.stylesheet.ts
+++ src/components/molecules/card-post/card-post.stylesheet.ts
@@ -1,3 +1,35 @@
 import {StyleSheet} from 'react-native';
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    width: '100%',
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    justifyContent: 'space-between',
+    flexWrap: 'wrap',
+  },
+  thumbnail: {
+    width: '25%',
+    flexGrow: 2,
+    marginRight: 10,
+  },
+  body: {
+    width: '70%',
+    justifyContent: 'space-between',
+    flexGrow: 3,
+  },
+  timeAgo: {
+    textAlign: 'right',
+  },
+  title: {
+    marginTop: 10,
+    fontWeight: 'bold',
+  },
+  bodyIntern: {
+    marginTop: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+});
--- src/components/molecules/card-post/card-post.tsx
+++ src/components/molecules/card-post/card-post.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import {Text, View, TouchableOpacity, Image} from 'react-native';
 import {convertEpochToTimeAgo} from '../../../utils/convertEpochToTime';
+import {styles} from './card-post.stylesheet';
 
 export const CardPost = ({content, onPress}) => {
   return (
-    <TouchableOpacity onPress={onPress}>
-      <Image source={{url: content.thumbnail}} />
-      <View>
-        <Text>{convertEpochToTimeAgo(content.created_utc)}</Text>
-        <Text>{content.title}</Text>
+    <TouchableOpacity style={styles.container} onPress={onPress}>
+      <Image style={styles.thumbnail} source={{url: content.thumbnail}} />
+      <View style={styles.body}>
+        <Text style={styles.timeAgo}>
+          {convertEpochToTimeAgo(content.created_utc)}
+        </Text>
+        <Text style={styles.title}>{content.title}</Text>
         <View />
-        <View>
+        <View style={styles.bodyIntern}>
           <Text>{content.name}</Text>
           <Text>Score:{content.score}</Text>
           <Text>Comments:{content.num_comments}</Text>
