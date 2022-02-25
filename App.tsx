488ae867c683e5734cb072550ac8b4fd466106e5
Alexis R <alexis.rojas@pragma.com.co>
2022-02-24 19:54:04 -0500

    testing

--- App.tsx
+++ App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
 import RootNavigation from './src/navigation/root';
 import {SafeAreaView, StyleSheet} from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <NavigationContainer>
-        <RootNavigation />
-      </NavigationContainer>
+      <RootNavigation />
     </SafeAreaView>
   );
 }
